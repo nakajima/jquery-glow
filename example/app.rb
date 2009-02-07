0b7f735761f05dd451f08eeb4aee14f766309c4d
Pat Nakajima <patnakajima@gmail.com>
2009-02-06 19:00:44 -0800

    various cleanups

--- example/app.rb
+++ example/app.rb
@@ -2,18 +2,8 @@ require 'rubygems'
 require 'sinatra'
 
 set :static, true
-set :public, File.join(File.dirname(__FILE__), '..', 'spec', 'screw-unit')
+set :public, File.join(File.dirname(__FILE__), '..', 'src')
 
 get '/' do
   erb :index
-end
-
-get '/jquery-glowing.js' do
-  content_type "text/javascript"
-  File.read(File.join(File.dirname(__FILE__), '..', 'src', 'jquery-glowing.js'))
-end
-
-get '/jquery.color.js' do
-  content_type "text/javascript"
-  File.read(File.join(File.dirname(__FILE__), '..', 'src', 'jquery.color.js'))
 end
--- src/jquery-glowing.js
+++ src/jquery-glowing.js
@@ -1,60 +1,69 @@
 (function() {
-  var currentRadius, multiplier, Options = {
-    RADIUS: 20,
-    DURATION: .5,
-    ORIGINAL_COLOR: '#777',
-    GLOW_COLOR: '#0af',
-    HALO_COLOR: '#059'
+  var currentRadius, multiplier;
+  
+  function parseOptions(options) {
+    return {
+      RADIUS:     (options.radius    || 20),
+      DURATION:   (options.duration  || .5),
+      TEXT_COLOR: (options.textColor || '#0af'),
+      HALO_COLOR: (options.haloColor || '#059')
+    }
   }
   
   function currentRadius(elem) {
-    if (s = elem.style['text-shadow']) {
-      return parseInt(s.match(/0 0 (\d+)px/));
+    if (prop = elem.style['text-shadow']) {
+      return parseInt(prop.match(/0 0 (\d+)px/));
     } else {
       return 0;
     }
   }
   
   jQuery.fx.step['textShadow'] = function(fx){
-		if ( fx.state == 0 ) {
+		if (fx.state == 0) {
 			fx.start = currentRadius(fx.elem);
-			fx.end = fx.end;
 		}
 		
 		with(fx.end) {
-		  multiplier = begin ? 1 : -1;
-      updatedRadius = parseInt(radius * fx.pos) * multiplier;
+      updatedRadius = begin ?
+        parseInt(radius * fx.pos) :
+        parseInt(radius - (radius * fx.pos))
   		fx.elem.style['text-shadow'] = color + ' 0 0 ' + updatedRadius + 'px';
+  		console.info(updatedRadius)
 		}
 	}
   
-  $.fn.addGlow = function addGlow(options) {
+  $.fn.addGlow = function addGlow(opts) {
+    var opts = parseOptions(opts || { });
+
     function startGlow() {
-      var element = $(this);
-      element.animate({
-        color: Options.GLOW_COLOR,
+      $(this).animate({
+        color: opts.TEXT_COLOR,
         textShadow: {
           begin: true,
-          color: Options.HALO_COLOR,
-          radius: Options.RADIUS
+          color: opts.HALO_COLOR,
+          radius: opts.RADIUS
         }
-      }, Options.DURATION * 1000);
+      }, opts.DURATION * 1000);
     }
 
     function startFade() {
-      var element = $(this);
-      element.animate({
-        color: Options.ORIGINAL_COLOR,
+      $(this).animate({
+        color: $(this).data('glow.originColor'),
         textShadow: {
           begin: false,
-          color: 0,
-          radius: 0
+          color: opts.HALO_COLOR,
+          radius: opts.RADIUS
         }
-      }, Options.DURATION * 1000);
-    }    
+      }, opts.DURATION * 1000);
+    }
     
-    $(this).bind('mouseenter', startGlow);
-    $(this).bind('mouseleave', startFade);
+    with($(this)) {
+      bind('mouseenter', startGlow);
+      bind('mouseleave', startFade);
+      data('glow.originColor', css('color'));
+    }
+    
+    return this;
   }
   
 })()
