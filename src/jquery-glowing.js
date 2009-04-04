b6cc85b87af62bc398027d9a9f60ab8efd99c926
Pat Nakajima <patnakajima@gmail.com>
2009-04-04 19:52:21 -0400

    Ensure proper cleanup

--- src/jquery-glowing.js
+++ src/jquery-glowing.js
@@ -1,6 +1,6 @@
 (function($) {
   var currentRadius, multiplier;
-  
+
   function parseOptions(options) {
     return {
       RADIUS:     (options.radius    || 20),
@@ -9,7 +9,7 @@
       HALO_COLOR: (options.haloColor || '#777')
     }
   }
-  
+
   function currentRadius(elem) {
     if (prop = elem.style['text-shadow']) {
       return parseInt(prop.match(/0 0 (\d+)px/));
@@ -17,20 +17,23 @@
       return 0;
     }
   }
-  
-  function stepTextShadow(fx) {
-		if (fx.state == 0) {
-			fx.start = currentRadius(fx.elem);
-		}
 
-		with(fx.end) {
-      updatedRadius = begin ?
-        parseInt(radius * fx.pos) :
-        parseInt(radius - (radius * fx.pos))
-  		fx.elem.style['text-shadow'] = color + ' 0 0 ' + updatedRadius + 'px';
-		}
+  function stepTextShadow(fx) {
+    if (fx.state == 0) {
+      fx.start = currentRadius(fx.elem);
+    }
+
+    updatedRadius = fx.end.begin ?
+      Math.max(Math.min(parseInt(fx.end.radius * fx.pos))) :
+      Math.max(Math.min(parseInt(fx.end.radius - (fx.end.radius * fx.pos))))
+
+    if (fx.end.begin || (fx.state < 1)) {
+      fx.elem.style['text-shadow'] = fx.end.color + ' 0 0 ' + updatedRadius + 'px';
+    } else {
+      fx.elem.style['text-shadow'] = 'none';
+    }
   }
-  
+
   function addGlow(opts) {
     var opts = parseOptions(opts || { });
 
@@ -55,16 +58,16 @@
         }
       }, opts.DURATION);
     }
-    
+
     with($(this)) {
       bind('mouseenter', startGlow);
       bind('mouseleave', startFade);
       data('glow.originColor', css('color'));
     }
-    
+
     return this;
   }
-  
+
   $.fx.step['textShadow'] = stepTextShadow;
   $.fn.addGlow = addGlow;
 })(jQuery);
